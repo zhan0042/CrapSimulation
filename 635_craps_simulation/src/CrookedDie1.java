d27f11904a2e5a5b2226ea055af5916c0c9bcef8
zhan0042 <32019402+zhan0042@users.noreply.github.com>
2017-09-30 10:26:13 -0500

    First commit

---
+++ 635_craps_simulation/src/CrookedDie1.java
@@ -0,0 +1,20 @@
+
+public class CrookedDie1 extends Die
+{
+	public CrookedDie1()
+	{
+		CrapsGame.println("  CrookedDie1's constructor called.");
+		
+	}
+
+	public int getCrookedLastRoll() // this OVERRIDES Die's getLastRoll(), replacing							// it!
+	{
+		return 3;
+	}
+
+	public String toString() // this OVERRIDES Die's toString()
+	{
+		return "A CrookedDie1 always rolling 3, and... " + super.toString();
+	}
+
+}
